fafb0e7b30baa36e60f24f8357afded6f419ab98
benjamincavada101115 <benjamin.cavada@ug.uchile.cl>
2026-04-09 00:09:44 -0400

    Feat: se agrega la clase Army con sus respectivas funciones

---
+++ src/main/army/Army.scala
@@ -0,0 +1,25 @@
+package army
+
+import soldier.Soldier
+
+import scala.collection.mutable.ArrayBuffer
+
+class Army(val name: String) {
+  val soldiers: ArrayBuffer[Soldier] = new ArrayBuffer[Soldier]
+
+  def join(soldier: Soldier): Unit = {
+    soldiers += soldier
+  }
+
+  def leave(soldier: Soldier): Unit = {
+    val index: Int = soldiers. indexOf(soldier)
+    if (index >= 0) {
+      soldiers.remove(index)
+    }
+  }
+
+  def showRoster(): Unit = {
+    val names = this.soldiers.map(_.name).mkString(", ")
+    println(s"Integrantes de ${this.name}: [${if (names.nonEmpty) names else "(vacío)"}]")
+  }
+}
